4d1900c74b00f2ce215d04625b0f1ab624fb3f11
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-09-07 18:37:52 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+travis_fold start foo
+  travis_cmd echo Foo1
+travis_fold end foo
+
+travis_fold start foo
+  travis_cmd echo Foo2
+travis_fold end foo
+
+travis_fold start foo
+  travis_cmd echo Foo3
+travis_fold end foo
+
