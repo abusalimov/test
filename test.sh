991d899ebd372728413001cd3cec4b7ad303c590
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2015-09-07 19:44:59 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
-travis_fold start foo
-  travis_cmd echo\ Foo1  --assert --echo
-travis_fold end foo
+travis_fold start asd.1
+  travis_fold start foo
+    travis_cmd echo\ Foo1  --assert --echo
+  travis_fold end foo
 
-travis_fold start bar
-  travis_cmd echo\ Bar  --assert --echo
-travis_fold end bar
+  travis_fold start bar
+    travis_cmd echo\ Bar  --assert --echo
+  travis_fold end bar
+travis_fold end asd.1
 
-travis_fold start foo
-  travis_cmd echo\ Foo2  --assert --echo
-travis_fold end foo
-
-travis_fold start foo
-  travis_cmd echo\ Foo3  --assert --echo
-travis_fold end foo
-
-travis_fold start baz
-  travis_cmd echo\ Baz  --assert --echo
-travis_fold end baz
+travis_fold start sdf.2
+  travis_fold start foo
+    travis_cmd echo\ Foo2  --assert --echo
+  travis_fold end foo
+  
+  travis_fold start baz
+    travis_cmd echo\ Baz  --assert --echo
+  travis_fold end baz
+travis_fold end sdf.2
